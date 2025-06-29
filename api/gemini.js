faa4a7df13a2545a343b4a2b0504a40a9450b5f2
AntonRize <Antonrize@gmail.com>
2025-06-30 06:06:05 +1000

    Update gemini.js

--- api/gemini.js
+++ api/gemini.js
@@ -11,8 +11,8 @@ const cors = {
 
 /* ---- приоритет моделей ---- */
 const MODELS = [
-  'gemini-1.5-pro',
-  'gemini-1.5-flash'
+  'gemini-2.5-pro',
+  'gemini-2.5-flash'
 ];
 
 /* ---- handler ---- */
